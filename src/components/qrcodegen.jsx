a03cec97466e1ff441bc7357cddde6c415955218
CaimeoAI <judiellezinke@yahoo.de>
2025-04-24 21:49:35 +0200

    added folderstructure and basic component containing the QR Code Generator, containing nonfunctional input and button

---
+++ src/components/qrcodegen.jsx
@@ -0,0 +1,14 @@
+
+
+export default function QRCodeGenerator() {
+
+    return (
+        <div>
+            <h1>QR Code Generator</h1>
+            <div>
+                <input type="text" name="qr-code" placeholder="Enter your value" />
+                <button>Generate</button>
+            </div>
+        </div>
+    )
+}
